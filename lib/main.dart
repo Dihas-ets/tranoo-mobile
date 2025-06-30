ed7e23cdc1bb02510608341dcba95f3d5bfe4564
glaurinda <157924586+glaurinda@users.noreply.github.com>
2025-06-30 16:24:28 +0100

    push avant back

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:tranoo/data/screens/first_page.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyApp());
+// }
+
 void main() => runApp(
   DevicePreview(
     enabled: !kReleaseMode,
