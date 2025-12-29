9c79580e1ef9a273f3377ec471313c092aba0619
glaurinda <157924586+glaurinda@users.noreply.github.com>
2025-12-29 13:10:05 +0100

    croix lecteur video

--- android/app/src/main/kotlin/tech/dihas/tramoo/MainActivity.kt
+++ android/app/src/main/kotlin/tech/dihas/tramoo/MainActivity.kt
@@ -1,5 +1,17 @@
 package tech.dihas.tramoo
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // Activer l'affichage bord à bord pour Android 15+ (SDK 35+)
+        // Cette API remplace les méthodes obsolètes setStatusBarColor, setNavigationBarColor, etc.
+        if (Build.VERSION.SDK_INT >= 35) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
+        super.onCreate(savedInstanceState)
+    }
+}
